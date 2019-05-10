0bc0a77325c93dcfd68a5ac42dfb799222afb0aa
AndrijaStanic <49030337+AndrijaStanic@users.noreply.github.com>
2019-05-10 11:01:10 +0200

    Still has password bug

--- Dating-SPA/src/app/register/register.component.ts
+++ Dating-SPA/src/app/register/register.component.ts
@@ -24,9 +24,10 @@ export class RegisterComponent implements OnInit {
 
   createRegisterForm() {
     this.registerForm = this.fb.group({
+      gender: ['male'],
       username: ['', Validators.required],
       knownAs: ['', Validators.required],
-      dateOfBirth: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
       city: ['', Validators.required],
       country: ['', Validators.required],
       password: ['', Validators.required, Validators.minLength(4), Validators.maxLength(14)],
